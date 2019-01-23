b09d3f5dbbe9bccb0e8a8e207e7281baed6a7c10
Henrik Metzger <henrikm@microsoft.com>
2019-01-23 14:14:37 -0800

    Removed empty scope used for testing

--- device-code-flow-console/MyInformation.cs
+++ device-code-flow-console/MyInformation.cs
@@ -46,7 +46,7 @@ namespace device_code_flow_console
         /// <summary>
         /// Scopes to request access to the protected Web API (here Microsoft Graph)
         /// </summary>
-        private static string[] Scopes { get; set; } = new string[] { "User.Read", "User.ReadBasic.All"}; // new string[] { "" };
+        private static string[] Scopes { get; set; } = new string[] { "User.Read", "User.ReadBasic.All"};
 
         /// <summary>
         /// URLs of the protected Web APIs to call (here Microsoft Graph endpoints)
